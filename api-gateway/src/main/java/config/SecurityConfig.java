30fd688eb374e715c035350570569e0738cc811a
Harmeet Singh Gorowada <harmeetsingh7276@gmail.com>
2023-07-27 18:06:27 +0530

    addded Spring Security

---
+++ api-gateway/src/main/java/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain spriSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
+        serverHttpSecurity.csrf()
+                .disable()
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/eureka/**")//This is to ignore the security for getting the static resources like css and js for the eureka server
+                        .permitAll()
+                        .anyExchange()
+                        .authenticated()
+                ).oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
+        return serverHttpSecurity.build();
+    }
+}
--- order-service/src/main/java/com/harmeetsinghgorowada/orderservice/repository/OrderRepository.java
+++ order-service/src/main/java/com/harmeetsinghgorowada/orderservice/repository/OrderRepository.java
@@ -2,6 +2,8 @@ package com.harmeetsinghgorowada.orderservice.repository;
 
 import com.harmeetsinghgorowada.orderservice.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OrderRepository extends JpaRepository<Order,Long> {
 }
